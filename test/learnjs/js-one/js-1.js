717de380126a7096e7e8b9e74fd0d2f04314b35a
pjs <fpsping@163.com>
2018-07-18 23:56:10 +0800

    js-1

--- test/learnjs/js-one/js-1.js
+++ test/learnjs/js-one/js-1.js
@@ -1,6 +1,5 @@
 var a = 1;
 console.log(a);
-
 console.log('*****');
 
 foo: {
@@ -10,7 +9,6 @@ foo: {
 }
 
 console.log(2);
-
 console.log('*****');
 console.log(typeof '11a');
 console.log(typeof null);
@@ -27,3 +25,26 @@ console.log(parseInt('1234'));
 console.log(parseInt('abc'));
 console.log(parseInt('1000', 10));
 console.log(parseInt('1000', 2));
+
+var obj = {
+    foo: 'Hello',
+    bar: 'World'
+};
+
+var obj = {a: 1, b: 2, c: 3};
+for(var i in obj) {
+    console.log('键名：', i);
+    console.log('键值：', obj[i]);
+}
+
+console.log(1 == true);
+console.log(1 == false);
+console.log(true ? 'T' : 'F');
+console.log(x = 5);
+void(x = 5);
+console.log(void(x = 5));
+console.log(123 + 'abc');
+
+if (!undefined && !null && !0 && !NaN && !'') {
+    console.log('true');
+}
