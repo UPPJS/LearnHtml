49a51bbd08b1b3e58c2fb3f2982c75f8f7cdd8d2
pjs <fpsping@163.com>
2018-07-21 23:50:38 +0800

    other

--- test/learnjs/js-one/js-1.js
+++ test/learnjs/js-one/js-1.js
@@ -26,19 +26,19 @@ console.log(parseInt('abc'));
 console.log(parseInt('1000', 10));
 console.log(parseInt('1000', 2));
 
-var obj = {
+var obj0 = {
     foo: 'Hello',
     bar: 'World'
 };
 
-var obj = {a: 1, b: 2, c: 3};
-for(var i in obj) {
+var obj0 = {a: 1, b: 2, c: 3};
+for(var i in obj0) {
     console.log('键名：', i);
-    console.log('键值：', obj[i]);
+    console.log('键值：', obj0[i]);
 }
 
-console.log(1 == true);
-console.log(1 == false);
+console.log(1 === true);
+console.log(1 === false);
 console.log(true ? 'T' : 'F');
 console.log(x = 5);
 void(x = 5);
@@ -48,3 +48,51 @@ console.log(123 + 'abc');
 if (!undefined && !null && !0 && !NaN && !'') {
     console.log('true');
 }
+
+var print = function x() {
+    console.log(typeof x);
+};
+print();
+
+function fib(num) {
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+    return fib(num - 2) + fib(num - 1);
+}
+console.log(fib(8));
+
+// function add(x, y) {
+//     return x + y;
+// }
+// var operator = add;
+// function a(op) {
+//     return op;
+// }
+// console.log(a(add)(1, 1));
+
+var f = function () {
+    console.log('1');
+};
+function f() {
+    console.log('2');
+}
+console.log(f());
+
+function f1() {}
+console.log(f1.name);
+
+console.log('*************');
+
+var p = 2;
+function f2(p) {
+    p = 3;
+}
+console.log(f2(p));
+
+Object.print = function (o) {console.log(o)};
+Object.prototype.print = function () {
+    console.log(this);
+};
+var obj1 = {};
+obj1.print();
+
---
+++ test/learnjs/js-one/js-2.js
@@ -0,0 +1,41 @@
+var obj = {
+    p1: 123,
+    p2: 456
+};
+
+console.log(Object.keys(obj));
+console.log(Object.getOwnPropertyNames(obj));
+
+var a = ['hello', 'world'];
+console.log(Object.keys(a));
+console.log(Object.getOwnPropertyNames(a));
+console.log(Object.keys(a).length);
+console.log(Object.getOwnPropertyNames(a).length);
+
+var b = {};
+console.log(b.toString());
+
+var arr = new Array(2);
+console.log(arr.length);
+console.log(arr);
+
+var arr0 = [1, 2, 3];
+console.log(typeof arr0);
+console.log(Array.isArray(arr0));
+
+var v1 = Number(123);
+var v2 = String('abc');
+var v3 = Boolean(true);
+console.log(typeof v1);
+console.log(v1 === 123 && v2 === 'abc' && v3 === true);
+
+console.log('***************');
+
+console.log(Date());
+
+var today = Date();
+console.log(today);
+console.log(Date.parse('1992-1-25'));
+console.log(Date.UTC(1992, 0, 1, 0, 0, 0, 0));
+
+console.log(Math.E);
