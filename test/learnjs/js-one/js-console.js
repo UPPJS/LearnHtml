6e558eb6add4f0300d924cb78cbb41b4d59f601a
pjs <fpsping@163.com>
2018-07-26 23:35:27 +0800

    other

---
+++ test/learnjs/js-one/js-console.js
@@ -0,0 +1,18 @@
+var nb = 11;
+var color = 'red';
+console.log('%d %s balloons', nb, color);
+
+console.log({foo: 'bar'});
+console.log(Date);
+
+console.info('Hello world!')
+
+console.error('Error: %s (%i)', 'Server is not responding', 500);
+
+var languages = [
+    {name: "JavaScript", fileExtension: ".js"},
+    {name: "TypeScript", fileExtension: ".ts"},
+    {name: "CoffeeScript", fileExtension: ".coffee"}
+];
+
+console.table(languages);
