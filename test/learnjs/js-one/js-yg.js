01f11c9ae31e3efbfb6b1c5f1f4413d7d3ccfb2d
pjs <fpsping@163.com>
2018-08-14 14:15:38 +0800

    js-yg

--- test/learnjs/js-one/js-yg.js
+++ test/learnjs/js-one/js-yg.js
@@ -1,10 +1,15 @@
 // 最后一遍了，不在修改
 
 // var xFirst = document.getElementById("xInput").value;
-var xFirst = 2700;
+// var xFirst = 2700;
+
+function xxFirst() {
+    const xFirst = document.getElementById("xInput").value;
+    return xFirst;
+}
 // 随机数生成
 function aRandom(min, max) {
-    var range, rand, num;
+    let range, rand, num;
     range = max -min;
     rand = Math.random();
     num = min + Math.round(rand * range);
@@ -15,39 +20,40 @@ function aRandom(min, max) {
 
 // 列表生成
 function xList() {
-    var xArr = [];  // 声明数组
+    let xArr = [];  // 声明数组
 
     for (var i = 0; i < 5; i++) {
-        var xx = aRandom(xFirst - 10, xFirst + 10);  // 依次让随机数等于 aa
+        var xx = aRandom(xxFirst() - 10, xxFirst() + 10);  // 依次让随机数等于 aa
         xArr.push(xx);  // 依次将 aa 写入数组
     }
     return xArr;
 }
 // var xxlist = xList();
 // console.log(xxlist[0]);
-console.log(xList());
+// console.log(xList());
 
 // 比较大小
 function xCompare() {
     let xxCompare;
-    if (Math.abs(Math.min(...xList()) - xFirst) >= Math.abs(Math.max(...xList()) - xFirst)) {
-        xxCompare = Math.min(...xList()) - xFirst;
+    if (Math.abs(Math.min(...xList()) - xxFirst()) >= Math.abs(Math.max(...xList()) - xxFirst())) {
+        xxCompare = Math.min(...xList()) - xxFirst();
     } else {
-        xxCompare = Math.max(...xList()) - xFirst;
+        xxCompare = Math.max(...xList()) - xxFirst();
     }
     return xxCompare;
 }
 
-console.log(xCompare());
+// console.log(xCompare());
 
 function xEnd() {
-    var xout1, xout2;
+    let xout1, xout2;
     console.log(xList()[0]);
     console.log(xCompare());
     xout1 = xList()[0];
     xout2 = xCompare();
+    document.getElementById("xout00").innerHTML = xxFirst();
     document.getElementById("xout01").innerHTML = xout1;
     document.getElementById("xout02").innerHTML = xout2;
 }
-xEnd();
+// xEnd();
 
--- test/testtest/yg-f/js-td-a.js
+++ test/testtest/yg-f/js-td-a.js
@@ -1,6 +1,8 @@
 // 解释：
 // firstone: a-h
 
+const firstone = document.getElementById("a-h").value;
+
 
 // 随机数生成
 function ahFunction(min, max) {
@@ -10,31 +12,39 @@ function ahFunction(min, max) {
     num = min + Math.round(rand * range);
     return num;
 }
-
-let Arr = [];  // 声明数组
-let firstone = document.getElementById("a-h").value;
-for (let i = 0; i < 5; i++) {
-    let aa = ahFunction(firstone - 10, firstone + 10);  // 依次让随机数等于 aa
-    Arr.push(aa);  // 依次将 aa 写入数组
-}
-
-let arra;
-if (Math.abs(Math.min(...Arr) - firstone) >= Math.abs(Math.max(...Arr) - firstone)) {
-    arra = Math.min(...Arr) - firstone;
-} else {
-    arra = Math.max(...Arr) - firstone;
-}
-
 function ahArr() {
-    // return Arr;
+    const firstone = document.getElementById("a-h").value;
 
+    let Arr = [];  // 声明数组
+    for (let i = 0; i < 5; i++) {
+        let aa = ahFunction(firstone - 10, firstone + 10);  // 依次让随机数等于 aa
+        Arr.push(aa);  // 依次将 aa 写入数组
+    }
+    return Arr;
+}
+
+function ahArra() {
+    const firstone = document.getElementById("a-h").value;
+
+    let arra;
+    if (Math.abs(Math.min(...ahArr()) - firstone) >= Math.abs(Math.max(...ahArr()) - firstone)) {
+        arra = Math.min(...ahArr()) - firstone;
+    } else {
+        arra = Math.max(...ahArr()) - firstone;
+    }
+    return arra;
+}
+
+function ahEnd() {
+    // let firstone = document.getElementById("a-h").value;
+    const firstone = document.getElementById("a-h").value;
     document.getElementById("a-h0").innerHTML = firstone;
-    document.getElementById("a-h1").innerHTML = Arr[0];
-    document.getElementById("a-h2").innerHTML = Arr[1];
-    document.getElementById("a-h3").innerHTML = Arr[2];
-    document.getElementById("a-h4").innerHTML = Arr[3];
-    document.getElementById("a-h5").innerHTML = Arr[4];
-    document.getElementById('a-a').innerHTML = arra;
+    document.getElementById("a-h1").innerHTML = ahArr()[0];
+    document.getElementById("a-h2").innerHTML = ahArr()[1];
+    document.getElementById("a-h3").innerHTML = ahArr()[2];
+    document.getElementById("a-h4").innerHTML = ahArr()[3];
+    document.getElementById("a-h5").innerHTML = ahArr()[4];
+    document.getElementById('a-a').innerHTML = ahArra();
 }
 
 // function ahA() {
